9885ae983c17416d5e1b3da2e6b8560cc7d52655
rk9534fjwr-ops <rk9534fjwr@gmail.com>
2025-10-21 21:46:06 +0900

    Delete src directory

--- src/app/api/users/route.ts
+++
@@ -1,8 +0,0 @@
-import { NextResponse } from 'next/server';
-import data from '@/resources/stub/users.json'; // ← JSONを直接import
-import type { ApiResponse } from '@/resources/types/UserData';
-
-export async function GET() {
-  const response = data as ApiResponse; // 型で保証される！
-  return NextResponse.json(response);
-}
